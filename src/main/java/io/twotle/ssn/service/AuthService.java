70064b578060f4bd3713a1dcc08c988c58d6be7a
Hyunju Yang <iam@2tle.io>
2022-09-12 21:22:38 +0900

    Minor: Auth Controller & Service - minor code update

--- src/main/java/io/twotle/ssn/service/AuthService.java
+++ src/main/java/io/twotle/ssn/service/AuthService.java
@@ -1,8 +1,12 @@
 package io.twotle.ssn.service;
 
+import io.twotle.ssn.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+    private UserRepository userRepository;
 
 }
